baddd9d9039067793ccc23253b9c24a1a68159fa
Eszter Laura Jónás <eszter.jonaska@gmail.com>
2021-04-29 18:50:28 +0200

    change to tm_device

--- mqttproxyproject/mqtt.py
+++ mqttproxyproject/mqtt.py
@@ -2,7 +2,7 @@ import paho.mqtt.client as mqtt
 
 address = 'mab.inf.elte.hu'  # mab.inf.elte.hu
 port = int(443)  # 443
-username = 'tm_user1'
+username = 'tm_device'
 password = '123'
 user = 'User1'
 
